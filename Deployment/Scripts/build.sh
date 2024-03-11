ee6d28b348958b3f2235618ae146c696991816bb
litebohorantso <154587991+litebohorantso@users.noreply.github.com>
2024-03-11 10:26:14 +0200

    Update build.sh
    Signed-off-by: litebohorantso <154587991+litebohorantso@users.noreply.github.com>

--- Deployment/Scripts/build.sh
+++ Deployment/Scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tar -czvf artifact.tar.gz spring-boot-hello-world-example
+tar -czvf artifact.tar.gz AWSDeployTP
 mkdir -p Deployment/Packages/source
 cp -R *.tar.gz Deployment/Packages/source
